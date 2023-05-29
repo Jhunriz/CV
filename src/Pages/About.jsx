3956e773993337b5d28f4eb0cb9d1a610fe99c92
Jhunriz <lalata.jhunriz.bscs2019@gmail.com>
2023-05-30 02:46:56 +0800

    Redesigning-page

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -5,9 +5,10 @@ import NavBar from "../components/NavBar";
 export default function About() {
   return (
     <>
-      {/* <NavBar /> */}
-      <div>
-        <h1>this the page for about</h1>
+      <div className="py-60 flex text-center">
+        <div className="">
+          <h1>this the page for about</h1>
+        </div>
       </div>
     </>
   );
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -9,8 +9,8 @@ export default function NavBar() {
   };
 
   return (
-    <div>
-      <nav className="bg-white border-gray-200 dark:bg-transparent">
+    <div className=" relative">
+      <nav className="bg-transparent fixed top-10 right-0 z-10 w-full md:right-10">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <a href="https://flowbite.com/" className="flex items-center">
             {/* <img src="" className="h-8 mr-3" alt="Flowbite Logo" /> */}
@@ -46,7 +46,7 @@ export default function NavBar() {
             <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-700 md:dark:bg-transparent dark:border-gray-700">
               <li>
                 <Link
-                  className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
+                  className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500 transform hover:scale-125 transition-all duration-300 ease-in-out"
                   to={"/"}
                 >
                   Home
@@ -54,7 +54,7 @@ export default function NavBar() {
               </li>
               <li>
                 <Link
-                  className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                  className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent transform hover:scale-125 transition-all duration-300 ease-in-out"
                   to={"/about"}
                 >
                   About
@@ -62,7 +62,7 @@ export default function NavBar() {
               </li>
               <li>
                 <Link
-                  className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                  className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent transform hover:scale-125 transition-all duration-300 ease-in-out"
                   to={"/skills"}
                 >
                   Skills
@@ -70,7 +70,7 @@ export default function NavBar() {
               </li>
               <li>
                 <Link
-                  className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                  className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent transform hover:scale-125 transition-all duration-300 ease-in-out"
                   to={"/porfolio"}
                 >
                   Porfolio
